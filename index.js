7c7d4c4c03bfb5490b18145a4a6845f8a780ee90
yusra shahid <yusrashahid111@gmail.com>
2022-11-07 14:03:36 +0500

    checking

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const app = express()
 const port = 3000
 
 const Pool = require('./dbconnection')
-const { default: pool } = require('./dbconnection')
+// const { default: pool } = require('./dbconnection')
 
 app.get('/', (request, response) => {
  response.json({info :' Node.js, and Express, and Postgres API'})
 })
 
-app.get('/testdb',async (request, response) => {
-//     dbPool.Query('select * from todoList,'(req,res) => {
+app.get('/testdb', async(request, response) => {
+    let res = await pool.query('select * from public.todoList')
+    console.log(res);
+    response.json({info : 'Node.js,Express, and Postgres API'})
+})
+
+// app.get('/testdb',async (request, response) => {
+// let res = await pool.query('select * from public.todoList')
 // console.log(res);
-//     })
-let res = await pool.query('select * from todoList')
-console.log(res);
-response.json({info :' Node.js, and Express, and Postgres API'})
-  })
+// response.json({info :' Node.js, and Express, and Postgres API'})
+//   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
